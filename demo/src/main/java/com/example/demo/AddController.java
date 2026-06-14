282c3c95caab4ef35883e5e29dfd82df62063f84
Mateusz Szybowski <matszyb@gmail.com>
2026-06-14 14:04:00 +0200

    CRUD wyszukiwarka i usuwanie

--- demo/src/main/java/com/example/demo/AddController.java
+++ demo/src/main/java/com/example/demo/AddController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,6 +19,18 @@ public class AddController {
     @GetMapping("/formularz")
     public String pokazFormularz() {
         return "formularz";
+    }
+
+    @GetMapping("/lista")
+    public String pokazListe(Model model) {
+        model.addAttribute("osoby", osobaRepo.findAll());
+        return "lista";
+    }
+
+    @PostMapping("/usun/{id}")
+    public String usunOsobe(@PathVariable Integer id) {
+        osobaRepo.deleteById(id);
+        return "redirect:/lista";
     }
 
     @PostMapping("/dodaj")
