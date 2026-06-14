779e54793c16f050bb125a48df85d32007691b33
Mateusz Szybowski <matszyb@gmail.com>
2026-06-14 11:49:46 +0200

    Initial commit - successful connection

---
+++ demo/src/main/java/com/example/demo/Osoba.java
@@ -0,0 +1,118 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+//import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name="KsiazkaTelefoniczna")
+public class Osoba {
+
+//    @jakarta.persistence.Id
+//    private Long id1;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String imie;
+    private String nazwisko;
+    @Column(name ="numer", nullable=false)
+    private String telefon;
+    private String email;
+    @Transient
+    boolean  nowy;
+
+    public Osoba(Integer id, String imie, String nazwisko, String telefon, String email, boolean nowy) {
+        this.id = id;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.telefon = telefon;
+        this.email = email;
+        this.nowy = nowy;
+    }
+
+    public Osoba() {
+
+    }
+
+//    public Long getId1() {
+//        return id1;
+//    }
+//
+//    public void setId1(Long id1) {
+//        this.id1 = id1;
+//    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isNowy() {
+        return nowy;
+    }
+
+    public void setNowy(boolean nowy) {
+        this.nowy = nowy;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Osoba{" +
+                "id=" + id +
+                ", imie='" + imie + '\'' +
+                ", nazwisko='" + nazwisko + '\'' +
+                ", telefon='" + telefon + '\'' +
+                ", email='" + email + '\'' +
+                ", nowy=" + nowy +
+                '}';
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
